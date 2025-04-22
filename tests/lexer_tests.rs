bde0493ccdc6be26c4b2862a265e258c764c82a4
Adel2411 <hadjarabadel.2411@gmail.com>
2025-04-22 05:38:41 +0100

    Changed lexer column position test

--- tests/lexer_tests.rs
+++ tests/lexer_tests.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod lexer_tests {
     use logos::Logos;
-    use rust_compiler::lexer::token::Token;
-    use rust_compiler::lexer::lexer_core::tokenize;
     use rust_compiler::lexer::error::LexicalErrorType;
+    use rust_compiler::lexer::lexer_core::tokenize;
+    use rust_compiler::lexer::token::Token;
 
     #[test]
     fn test_keywords() {
@@ -92,9 +92,18 @@ mod lexer_tests {
     #[test]
     fn test_string_literals() {
         let mut lexer = Token::lexer("\"hello\" \"hello world\" \"123\" \"\"");
-        assert_eq!(lexer.next(), Some(Ok(Token::StringLiteral("hello".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::StringLiteral("hello world".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::StringLiteral("123".to_string()))));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::StringLiteral("hello".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::StringLiteral("hello world".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::StringLiteral("123".to_string())))
+        );
         assert_eq!(lexer.next(), Some(Ok(Token::StringLiteral("".to_string()))));
         assert_eq!(lexer.next(), None);
     }
@@ -103,11 +112,26 @@ mod lexer_tests {
     fn test_valid_identifiers() {
         let mut lexer = Token::lexer("x Variable x123 test_var a_b_c Some123thing");
         assert_eq!(lexer.next(), Some(Ok(Token::Identifier("x".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("Variable".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("x123".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("test_var".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("a_b_c".to_string()))));
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("Some123thing".to_string()))));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("Variable".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("x123".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("test_var".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("a_b_c".to_string())))
+        );
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("Some123thing".to_string())))
+        );
         assert_eq!(lexer.next(), None);
     }
 
@@ -118,11 +142,14 @@ mod lexer_tests {
         assert_eq!(lexer.next(), Some(Ok(Token::Identifier("b".to_string()))));
         assert_eq!(lexer.next(), Some(Ok(Token::Identifier("c".to_string()))));
     }
-    
+
     #[test]
     fn test_multiline_comments() {
         let mut lexer = Token::lexer("start\n{-- line1\nline2\nline3 --}\nend");
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("start".to_string()))));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("start".to_string())))
+        );
         assert_eq!(lexer.next(), Some(Ok(Token::Identifier("end".to_string()))));
         assert_eq!(lexer.next(), None);
     }
@@ -151,7 +178,7 @@ mod lexer_tests {
         // Test integer literal above max i16
         let mut lexer = Token::lexer("32768");
         assert_eq!(lexer.next(), Some(Err(())));
-        
+
         // Test integer literal below min i16
         let mut lexer = Token::lexer("(-32769)");
         assert_eq!(lexer.next(), Some(Err(())));
@@ -163,16 +190,16 @@ mod lexer_tests {
         let mut lexer = Token::lexer("+123");
         assert_eq!(lexer.next(), Some(Ok(Token::Plus)));
         assert_eq!(lexer.next(), Some(Ok(Token::IntLiteral(123))));
-        
+
         let mut lexer = Token::lexer("-456");
         assert_eq!(lexer.next(), Some(Ok(Token::Minus)));
         assert_eq!(lexer.next(), Some(Ok(Token::IntLiteral(456))));
-        
+
         // Test un-parenthesized signed floats
         let mut lexer = Token::lexer("+123.45");
         assert_eq!(lexer.next(), Some(Ok(Token::Plus)));
         assert_eq!(lexer.next(), Some(Ok(Token::FloatLiteral(123.45))));
-        
+
         let mut lexer = Token::lexer("-456.78");
         assert_eq!(lexer.next(), Some(Ok(Token::Minus)));
         assert_eq!(lexer.next(), Some(Ok(Token::FloatLiteral(456.78))));
@@ -206,7 +233,10 @@ mod lexer_tests {
     #[test]
     fn test_mixed_valid_and_invalid() {
         let mut lexer = Token::lexer("valid1 invalid__id 12345");
-        assert_eq!(lexer.next(), Some(Ok(Token::Identifier("valid1".to_string()))));
+        assert_eq!(
+            lexer.next(),
+            Some(Ok(Token::Identifier("valid1".to_string())))
+        );
         assert_eq!(lexer.next(), Some(Err(()))); // invalid__id
         assert_eq!(lexer.next(), Some(Ok(Token::IntLiteral(12345))));
         assert_eq!(lexer.next(), None);
@@ -216,7 +246,7 @@ mod lexer_tests {
     fn test_tokenize_function() {
         let source = "let x := 10;";
         let (tokens, errors) = tokenize(source);
-        
+
         assert_eq!(errors.len(), 0);
         assert_eq!(tokens.len(), 5);
         assert_eq!(tokens[0].kind, Token::Let);
@@ -230,14 +260,14 @@ mod lexer_tests {
     fn test_tokenize_with_errors() {
         let source = "let x := (+10); # This is not a valid comment";
         let (tokens, errors) = tokenize(source);
-        
+
         assert_eq!(tokens.len(), 11);
         assert_eq!(tokens[0].kind, Token::Let);
         assert_eq!(tokens[1].kind, Token::Identifier("x".to_string()));
         assert_eq!(tokens[2].kind, Token::Assign);
         assert_eq!(tokens[3].kind, Token::IntLiteral(10));
         assert_eq!(tokens[4].kind, Token::Semicolon);
-        
+
         assert_eq!(errors.len(), 1);
         assert_eq!(errors[0].error_type, LexicalErrorType::InvalidToken);
     }
@@ -266,20 +296,20 @@ mod lexer_tests {
         let mut lexer = Token::lexer("{-- comment --} <!- another -!>");
         assert_eq!(lexer.next(), None);
     }
-    
+
     #[test]
     fn test_position_information() {
         let source = "let\nx := 10";
         let (tokens, _) = tokenize(source);
-        
+
         assert_eq!(tokens[0].line, 1);
-        assert_eq!(tokens[0].column, 0);
-        
+        assert_eq!(tokens[0].column, 1);
+
         assert_eq!(tokens[1].line, 2);
-        assert_eq!(tokens[1].column, 0);
-        
+        assert_eq!(tokens[1].column, 1);
+
         assert_eq!(tokens[2].line, 2);
-        assert_eq!(tokens[2].column, 2);
+        assert_eq!(tokens[2].column, 3);
     }
 
     #[test]
@@ -310,7 +340,7 @@ MainPrgm
         let (tokens, errors) = tokenize(source);
         assert_eq!(errors.len(), 0);
         assert!(tokens.len() > 0);
-        
+
         // Verify first few tokens
         assert_eq!(tokens[0].kind, Token::MainPrgm);
         assert_eq!(tokens[1].kind, Token::Var);
